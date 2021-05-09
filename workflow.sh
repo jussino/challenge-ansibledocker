04574857617094e85f89514f595e66d0ab3fc2ba
Cloud User <centos@ip-172-31-13-112.us-east-2.compute.internal>
2021-05-09 22:34:34 +0000

    new file:   ansible.cfg 	new file:   dyn-inventory_aws_ec2.yml 	modified:   inventory 	modified:   launch-ec2.yml 	new file:   workflow.sh

---
+++ workflow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# launch Dockerhost server
+#ansible-playbook ./launch-ec2.yml
+
+# Create inventory file for environment
+ansible-inventory -i dyn-inventory_aws_ec2.yml --list --yaml|grep -i public_dns_name|awk {'print $2'}|uniq >> inventory
+
+# Install Dokcerhost
+ansible-playbook ./install-docker.yml
+
+# Build Application and Docker image
+ansible-playbook ./docker-container.yml
