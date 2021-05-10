8d980c958e871849236cf5d6ed7f9c7acb125aea
Cloud User <centos@ip-172-31-13-41.us-east-2.compute.internal>
2021-05-10 10:57:11 +0000

    modified:   ansible.cfg 	deleted:    challenge-api-0.1.0.jar 	deleted:    install-docker.yml-OLD 	modified:   inventory 	deleted:    inventory-orig 	modified:   workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -4,6 +4,7 @@
 #ansible-playbook ./launch-ec2.yml
 
 # Create inventory file for environment
+sed -i '/targets/!d' inventory
 ansible-inventory -i dyn-inventory_aws_ec2.yml --list --yaml|grep -i public_dns_name|awk {'print $2'}|uniq >> inventory
 
 # Install Dokcerhost
