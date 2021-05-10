904d451e81faed28d3f9f93a6a0204579778ebe7
Cloud User <centos@ip-172-31-13-41.us-east-2.compute.internal>
2021-05-10 16:41:00 +0000

    Final

--- workflow.sh
+++ workflow.sh
@@ -3,6 +3,9 @@
 # launch Dockerhost server
 #ansible-playbook ./launch-ec2.yml
 
+# Wait for instances to reachable
+ansible-playbook test.yml
+
 # Create inventory file for environment
 sed -i '/targets/!d' inventory
 ansible-inventory -i dyn-inventory_aws_ec2.yml --list --yaml|grep -i public_dns_name|awk {'print $2'}|uniq >> inventory
@@ -12,3 +15,9 @@ ansible-playbook ./install-docker.yml
 
 # Build Application and Docker image
 ansible-playbook ./docker-container.yml
+
+# Install validation tools
+ansible-playbook ./validation-tools.yml
+
+# Enable both .ssh/authorized_keys and .ssh/authorized_keys2
+ansible-playbook ./change-sshd_config.yml
